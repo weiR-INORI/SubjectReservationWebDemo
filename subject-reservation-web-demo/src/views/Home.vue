40609fa9f03dc6144038f94dc2fcbeb294da9a14
Kamolwish Woramethaleot <kamolwish.riew189521@mail.kmutt.ac.th>
2021-08-07 14:11:58 +0700

    update title to be netlify

--- subject-reservation-web-demo/src/views/Home.vue
+++ subject-reservation-web-demo/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <br />
     <p style="font-family: Roboto; font-size: 18pt">
-      SIT Subject Reservation Web 2022
+      SIT Subject Reservation Web A.D.2022
     </p>
     <br />
     <v-row justify="center" align="top" no-gutters style="height: 150px">
